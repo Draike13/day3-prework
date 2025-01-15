6d5a6c76daab109a58435f8ad1f5743b219fd448
Draike13 <tmbiri91@gmail.com>
2025-01-15 12:19:21 -0600

    add difficulty select and save

--- webpage.js
+++ webpage.js
@@ -1,5 +1,6 @@
 let count = 0;
-console.log(count);
+let selectedDifficulty;
+
 function add() {
   count += 1;
 }
@@ -7,12 +8,31 @@ function add() {
 function check() {
   console.log(count);
 }
+
 function final() {
   alert(`The final count is ${count} \n Goodbye`);
   //   return (count = 0);
   location.reload();
 }
+
 function getNumber() {
   let num = document.getElementById('number').value;
   return (count = 0 + Number(num));
+}
+
+// checks to see which radio is selected for difficulty
+// saves selection to difficulty variable
+function isChecked() {
+  let difficultyCheck;
+  if (document.getElementById('easy').checked === true) {
+    console.log('easy');
+    difficultyCheck = 'E';
+  } else if (document.getElementById('medium').checked === true) {
+    console.log('medium');
+    difficultyCheck = 'M';
+  } else if (document.getElementById('hard').checked === true) {
+    console.log('hard');
+    difficultyCheck = 'H';
+  }
+  return (selectedDifficulty = difficultyCheck);
 }
